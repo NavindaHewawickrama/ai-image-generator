a46b169fd22b533c4437c3eef15894bfbdbb04d4
NavindaHewawickrama <hewawickraman@gmail.com>
2025-01-20 23:49:49 +0530

    container and wrapper elements created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import styled, { ThemeProvider } from "styled-components";
 import {darkTheme} from "./utils/Theme"
 
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  jsutify-content: space-between;
+  flex: 3;
+  `;
+
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      Hello World
+      <Container>
+        <Wrapper>
+          Hello
+        </Wrapper>
+      </Container>
     </ThemeProvider>
   );
 }
