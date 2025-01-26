edc3ff682becbc91443c086a70287ff85d2ba37b
NavindaHewawickrama <hewawickraman@gmail.com>
2025-01-26 15:53:57 +0530

    front end finished

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import GenerateImage from "../components/form/GenerateImage";
+import GeneratedImageCard from "../components/cards/GeneratedImageCard";
 
 const Container = styled.div`
-  padding: 30px 30px;
-  padding-bottom: 200px;
+  padding: 20px 30px;
+  padding-bottom: 50px;
   height: 100%;
+  justify-content: center;
+  align-items: center;
   overflow-y: scroll;
   display: flex;
-  flex-direction: column;
-  align-items: center;
   gap: 20px;
   @media (max-width: 768px) {
     padding: 6px 10px;
@@ -16,51 +18,40 @@ const Container = styled.div`
   background: ${({ theme }) => theme.background};
 `;
 
-const HeadLine = styled.div`
-  font-size: 34px;
-  font-weight: 500;
-  color: ${({ theme }) => theme.text_primary};
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const Span = styled.div`
-  font-size: 30px;
-  font-weight: 800;
-  color: ${({ theme }) => theme.secondary};
-`;
-
 const Wrapper = styled.div`
-  width: 100%;
-  max-width: 1400px;
-  padding: 32px 0px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CardWrapper = styled.div`
-  display: grid;
-  gap: 20px;
-
-  @media (min-width: 1200px) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-
-  @media (min-width: 640px) and (max-width: 1199px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  @media (max-width: 639px) {
-    grid-template-columns: repeat(2, 1fr);
+  gap: 8%;
+  flex: 1;
+  max-width: 1200px;
+  height: fit-content;
+  @media (max-width: 768px) {
+    flex-direction: column;
   }
 `;
 
 const CreatePost = () => {
-    return (
-        <Container> Create Post</Container>
-    );
-}
+  const [generateImageLoading, setGenerateImageLoading] = useState(false);
+  const [createPostLoading, setcreatePostLoading] = useState(false);
+  const [post, setPost] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  return (
+    <Container>
+      <Wrapper>
+        <GenerateImage
+          createPostLoading={createPostLoading}
+          setcreatePostLoading={setcreatePostLoading}
+          generateImageLoading={generateImageLoading}
+          setGenerateImageLoading={setGenerateImageLoading}
+          post={post}
+          setPost={setPost}
+        />
+        <GeneratedImageCard loading={generateImageLoading} src={post.photo} />
+      </Wrapper>
+    </Container>
+  );
+};
 
 export default CreatePost;
