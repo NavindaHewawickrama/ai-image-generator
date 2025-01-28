f1f8d2a2246bf1ed9c831d8924f048978f9094c1
NavindaHewawickrama <hewawickraman@gmail.com>
2025-01-28 08:37:13 +0530

    posts schema

---
+++ server/controllers/Posts.js
@@ -0,0 +1,5 @@
+import { createError } from '../error.js';
+import Post from '../models/Post.js';
+import * as dotenv from 'dotenv';
+import { createError } from '../error.js';
+import {v2 as cloudinary} from 'cloudinary';
